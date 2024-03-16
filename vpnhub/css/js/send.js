29fe6e3fda71987461a20231b6c416f1ecfd130e
Salari-info <154739444+Salari-info@users.noreply.github.com>
2024-03-15 22:20:51 -0700

    Add files via upload

---
+++ vpnhub/css/js/send.js
@@ -0,0 +1,20 @@
+
+function sendmessge () {
+    var text = document.getElementById('messagef').value
+    var name = document.getElementById('name').value
+    document.getElementById('messagef').value = ''
+    document.getElementById('name').value = ''
+    var text_send = "💬 | #پیام_جدید \n نام:\n " + String(name) + "\nمتن پیام : \n " + String(text);
+    try {
+        $.ajax({
+            type:'post',
+            mode: "cors",
+            cache: "no-cache",
+            url:"https://tapi.bale.ai/botwT9ArKZEC8Pxy7mSjvMPHsPj6JiJlIEQDX7P7MOT/sendMessage",
+            data:{chat_id:"915303220", text:String(text_send)},
+        });
+        
+    } catch {
+        alert('Error in send Feed Back')
+    }
+}
